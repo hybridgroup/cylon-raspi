cb3aac4a60e6a06608cf4450b35be23107554061
edgarsilva <edgar.osc@gmail.com>
2014-06-11 10:50:01 -0500

    Change angleLimits param in servo examples for just limits.

--- examples/servo/servo.js
+++ examples/servo/servo.js
@@ -2,7 +2,12 @@ var Cylon = require('cylon');
 
 Cylon.robot({
   connection: { name: 'raspi', adaptor: 'raspi', port: '/dev/ttyACM0' },
-  device: { name: 'servo', driver: 'servo', pin: 11 },
+  device: {
+    name: 'servo',
+    driver: 'servo',
+    pin: 11,
+    limits: { bottom: 20, top: 160 }
+  },
 
   work: function(my) {
     var angle = 30,
