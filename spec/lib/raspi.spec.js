22463ae9f46ccb99486f58c57a85714b89e33f2e
edgarsilva <edgar.osc@gmail.com>
2014-12-18 18:17:29 -0600

    Move read cpuinfo to own method for easy stubbing, fix initial WIP on tests.

--- spec/lib/raspi.spec.js
+++ spec/lib/raspi.spec.js
@@ -1,9 +1,42 @@
+/*jshint multistr: true */
+
 'use strict';
 
 var Adaptor = source('raspi');
+var fs = source('raspi');
 
 describe('Cylon.Adaptors.Raspi', function() {
-  var raspi = new Adaptor();
+  var adaptor;
 
-  it("needs tests");
+  beforeEach(function() {
+
+    adaptor = new Adaptor();
+
+    stub(adaptor, "_cpuinfo").returns("\
+      Hardware	: BCM2708\
+      Revision	: 000e\
+      Serial		: 00000000ca44634a"
+    );
+  });
+
+  describe("#constructor", function() {
+    it('should set this.pins as an object', function() {
+      expect(adaptor.pins).to.be.eql({});
+    });
+  });
+
+
+  describe("#connect", function() {
+    var callback;
+
+    beforeEach(function() {
+      callback = spy();
+
+      adaptor.connect(callback);
+    });
+
+    it('should set the this.revision number to rev2', function() {
+      expect(adaptor.revision).to.be.eql('rev2');
+    });
+  });
 });
