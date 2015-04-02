16f5c847d74eaa172b0dd5ba3209fbcb1df7dc2e
Andrew Stewart <andrew@stwrt.ca>
2015-04-02 16:19:59 -0700

    Begin work on test coverage of raspi.js

--- spec/lib/raspi.spec.js
+++ spec/lib/raspi.spec.js
@@ -1,29 +1,49 @@
 // jshint expr:true
 "use strict";
 
-var Adaptor = source("raspi");
+var Cylon = require("cylon");
+
+var Raspi = source("raspi");
 
 describe("Cylon.Adaptors.Raspi", function() {
   var adaptor;
 
   beforeEach(function() {
-    adaptor = new Adaptor();
-
-    var cpuInfo = [
-      "Hardware	: BCM2708",
-      "Revision	: 000e",
-      "Serial		: 00000000ca44634a"
-    ].join("\n");
-
-    stub(adaptor, "_cpuinfo").returns(cpuInfo);
+    adaptor = new Raspi();
   });
 
-  describe("#constructor", function() {
-    it("should set this.pins as an object", function() {
+  it("is an instance of Cylon.Adaptor", function() {
+    expect(adaptor).to.be.an.instanceOf(Raspi);
+    expect(adaptor).to.be.an.instanceOf(Cylon.Adaptor);
+  });
+
+  describe("constructor", function() {
+    it("sets @board to empty string by default", function() {
+      expect(adaptor.board).to.be.eql("");
+    });
+
+    it("sets @pins to an empty object by default", function() {
       expect(adaptor.pins).to.be.eql({});
+    });
+
+    it("sets @pwmPins to an empty object by default", function() {
+      expect(adaptor.pwmPins).to.be.eql({});
+    });
+
+    it("sets @i2cDevices to an empty object by default", function() {
+      expect(adaptor.i2cDevices).to.be.eql({});
     });
   });
 
+  describe("#commands", function() {
+    it("is an array of Raspi commands", function() {
+      expect(adaptor.commands).to.be.an("array");
+
+      adaptor.commands.forEach(function(cmd) {
+        expect(cmd).to.be.a("string");
+      });
+    });
+  });
 
   describe("#connect", function() {
     var callback;
@@ -31,11 +51,68 @@ describe("Cylon.Adaptors.Raspi", function() {
     beforeEach(function() {
       callback = spy();
 
+      adaptor.proxyMethods = spy();
+      adaptor._cpuinfo = stub().returns("Revision : 000e");
       adaptor.connect(callback);
     });
 
-    it("should set the this.revision number to rev2", function() {
+    it("proxies methods to the board", function() {
+      expect(adaptor.proxyMethods).to.be.calledWith(
+        adaptor.commands,
+        adaptor.board,
+        adaptor
+      );
+    });
+
+    it("sets @revision based on CPU info", function() {
       expect(adaptor.revision).to.be.eql("rev2");
+
+      adaptor._cpuinfo.returns("Revision : 0001");
+      adaptor.connect(callback);
+      expect(adaptor.revision).to.be.eql("rev1");
+
+      adaptor._cpuinfo.returns("Revision : 0100");
+      adaptor.connect(callback);
+      expect(adaptor.revision).to.be.eql("rev3");
+    });
+
+    it("sets @i2cInterface based on CPU info", function() {
+      expect(adaptor.i2cInterface).to.be.eql("/dev/i2c-1");
+
+      adaptor._cpuinfo.returns("Revision : 0001");
+      adaptor.connect(callback);
+      expect(adaptor.i2cInterface).to.be.eql("/dev/i2c-0");
+    });
+
+    it("triggers the callback", function() {
+      expect(callback).to.be.called;
+    });
+  });
+
+  describe("#disconnect", function() {
+    var disconnect, callback;
+
+    beforeEach(function() {
+      disconnect = spy();
+      callback = spy();
+
+      adaptor.on("disconnect", disconnect);
+
+      adaptor._disconnectPins = spy();
+
+      adaptor.disconnect(callback);
+    });
+
+    it("disconnects all pins", function() {
+      expect(adaptor._disconnectPins).to.be.called;
+    });
+
+    it("emits 'disconnect'", function() {
+      expect(disconnect).to.be.called;
+    });
+
+    it("triggers the callback", function() {
+      expect(callback).to.be.called;
     });
   });
 });
